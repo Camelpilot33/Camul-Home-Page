6e11379efb9745e24f0b770b67b85959714f669f
Samuel Sastry <78936519+Camelpilot33@users.noreply.github.com>
2021-12-01 21:22:11 -0800

    Day2!

--- AdventOfCode.js
+++ AdventOfCode.js
@@ -23,3 +23,47 @@ for (let i = 1; i < str.length-1; i++) {
 	a=str[i]+str[i-1]+str[i+1];
 }
 console.log(b);
+//d2p1
+var x=0;
+var y=0;
+var str=`<input>`;
+str=str.split("\n");
+for (var i in str) {
+	str[i]=str[i].split(" ")
+	str[i][1]=parseInt(str[i][1]);
+	switch (str[i][0]) {
+		case "forward":
+		x+=str[i][1];
+		break;
+		case "up":
+		y-=str[i][1];
+		break;
+		case "down":
+		y+=str[i][1];
+		break;
+	}
+}
+console.log(x*y);
+//d2p2
+var x=0;
+var y=0;
+var aim=0;
+var str=`<input>`;
+str=str.split("\n");
+for (var i in str) {
+	str[i]=str[i].split(" ")
+	str[i][1]=parseInt(str[i][1]);
+	switch (str[i][0]) {
+		case "forward":
+		x+=str[i][1];
+		y+=str[i][1]*aim;
+		break;
+		case "up":
+		aim-=str[i][1];
+		break;
+		case "down":
+		aim+=str[i][1];
+		break;
+	}
+}
+console.log(x*y);
