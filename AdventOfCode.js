a636210e2fdfc7a2db2f9466188298e6bcb3aeae
Samuel Sastry <78936519+Camelpilot33@users.noreply.github.com>
2021-12-07 07:25:14 -0800

    d7p1

--- AdventOfCode.js
+++ AdventOfCode.js
@@ -434,3 +434,23 @@ for (var i=0;i<256;i++) {
 }
 fishies=fishies.reduce((a, b) => a + b, 0)
 console.log(fishies)
+//d7p1
+const input = ``.split(",").map(element => parseInt(element))
+function median(values){
+  values.sort(function(a,b){
+    return a-b;
+  });
+  var half = Math.floor(values.length / 2);
+  if (values.length % 2)
+    return values[half];
+  return (values[half - 1] + values[half]) / 2.0;
+}
+function fuel(values,value) {
+	value=median(values)
+	s=0
+	for (i=0;i<values.length;i++) {
+		s+=Math.abs(value-values[i])
+	}
+	return s
+}
+console.log(fuel(input))
