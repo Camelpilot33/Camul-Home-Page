af97c3524f4936e8aad89c9e97c8ac26cd4f8451
Samuel Sastry <78936519+Camelpilot33@users.noreply.github.com>
2022-07-16 10:36:15 -0700

    Update Last digit of a large number.js

--- toy/cw/Last digit of a large number.js
+++ toy/cw/Last digit of a large number.js
@@ -1,20 +1,12 @@
 var lastDigit = function(a, b){
-	var o={
-		0:[0],
-		1:[1],
-		2:[2,4,8,6],
-		3:[3,9,7,1],
-		4:[4,6],
-		5:[5],
-		6:[6],
-		7:[7,9,3,1],
-		8:[8,4,2,6],
-		9:[9,1],
-	}
+	const f=(a,b=1)=>Number(String(a)[String(a).length-b])
   if (b=="0")return 1
-	a=Number(a[a.length-1])
-	if (o[a].length==1) return a
-	b=b>9?b[b.length-2]+b[b.length-1]:b
-	let c=(b%o[a].length)-1==-1?o[a].length-1:(b%o[a].length)-1
-	return o[a][c]
+	a=f(a)
+	let p=[a]
+	let r=2
+	while (f(a**r)!=a){p.push(f(a**r));r++}
+	let l=p.length
+	b=b>9?String(f(b,2))+f(b):b
+	let c=(b%l)-1==-1?l-1:(b%l)-1
+	return p[c]
 }
