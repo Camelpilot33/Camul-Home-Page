71b8d9e7071aab4cf3a32061437cf787006e5426
Samuel Sastry <78936519+Camelpilot33@users.noreply.github.com>
2022-10-20 10:12:07 -0700

    Create baseConverter.js

---
+++ toy/CST/baseConverter.js
@@ -0,0 +1,18 @@
+const conv=(a,b,c)=>{
+	let n="0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	a=a.split("").map(e=>e.toUpperCase()).join("")
+	let x=0,p=0
+	while (a.length>0) {
+		x+=n.indexOf(a[a.length-1])*(b**p)
+		a=a.substring(0,a.length-1)
+		p++
+	}
+	let h=""
+	let i=0
+	while (x>0) {
+		h+=n[String(x%c)]
+		x=(x-(x%c))/c
+	}
+	return h
+}
+console.log(conv("10",10,16))
